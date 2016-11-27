8b3ecde72a99d76d390673c2052dae89e18a4a75
dgharbieh <duaa_abu_gharbieh@hotmail.com>
2016-11-27 17:49:47 +0200

    commit

---
+++ DynamicDatasource/Pipeline/CustomXmlBasedRenderingParser.cs
@@ -0,0 +1,31 @@
+﻿using System.Xml.Linq;
+using Sitecore;
+using Sitecore.Data.Items;
+using Sitecore.Mvc.Presentation;
+
+namespace DynamicDatasource.Pipeline
+{
+    public class CustomXmlBasedRenderingParser : XmlBasedRenderingParser
+    {
+        public override Rendering Parse(XElement node, bool parseChildNodes)
+        {
+            Rendering rendering = base.Parse(node, parseChildNodes);
+            ResolveRenderingItemDataSource(rendering);
+            return rendering;
+        }
+
+        private static void ResolveRenderingItemDataSource(Rendering rendering)
+        {
+            if (rendering.DataSource != null && rendering.DataSource.StartsWith("query:"))
+            {
+                string query = rendering.DataSource.Substring("query:".Length);
+                Item contextItem = Context.Item;
+                Item queryItem = contextItem.Axes.SelectSingleItem(query);
+                if (queryItem != null)
+                {
+                    rendering.DataSource = queryItem.Paths.FullPath;
+                }
+            }
+        }
+    }
+}
---
+++ DynamicDatasource/Pipeline/RegisterCustomXmlBasedRenderingParser.cs
@@ -0,0 +1,14 @@
+﻿using Sitecore.Mvc.Configuration;
+using Sitecore.Mvc.Presentation;
+using Sitecore.Pipelines;
+
+namespace DynamicDatasource.Pipeline
+{
+    public class RegisterCustomXmlBasedRenderingParser
+    {
+        public virtual void Process(PipelineArgs args)
+        {
+            MvcSettings.RegisterObject<XmlBasedRenderingParser>(() => new CustomXmlBasedRenderingParser());
+        }
+    }
+}
